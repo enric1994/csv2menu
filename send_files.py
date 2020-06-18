50d2bf54c768aced09bbac4322e48e1c871a5a69
David Azcona <david.azcona@zalando.ie>
2020-06-18 11:30:10 +0100

    refactoring of the code using Pandas functions and more readable HTML inline coding

--- send_files.py
+++ send_files.py
@@ -1,9 +1,10 @@
 import requests
 
+filename = 'examples/menu.csv'
 headers = {'Content-Type': 'text/plain'}
 url = "http://localhost:5000/menu"
 
-with open('menu.csv', 'rb') as file:
-    req = requests.post(url, data=file, headers=headers)
+with open(filename, 'rb') as f:
+    request = requests.post(url, data=f, headers=headers)
 
-print(req)
+print(request)
